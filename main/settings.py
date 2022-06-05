6a9af32ef6cd01738eeac1feb6a5ffedea4abcae
mikaildokgoz <mikaildokgoz@gmail.com>
2022-06-05 22:16:48 +0300

    settings file update

--- main/settings.py
+++ main/settings.py
@@ -26,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("ERROR")
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
